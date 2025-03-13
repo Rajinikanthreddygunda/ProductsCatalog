e5697e25ec9d433ad4faf05ca9522ae3dda1b9b9
venkatarajaboina <venkatachalapathirao98@gmail.com>
2025-03-13 19:28:45 -0400

    Implemented Create Call

--- Products/routes.go
+++ Products/routes.go
@@ -5,18 +5,7 @@ import (
 	"net/http"
 )
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	// Do nothing or return an error
-	http.Error(w, "Route not implemented", http.StatusNotImplemented)
-}
-
-// func RegisterRoutes(db *sql.DB) {
-// 	http.HandleFunc("/create", dummyHandler)
-// 	http.HandleFunc("/books", dummyHandler)
-// }
-
 func RegisterRoutes(db *sql.DB) {
-	//h := NewHandler(db)
-	http.HandleFunc("/products", testHandler)
-
+	h := NewHandler(db)
+	http.HandleFunc("/products/", h.DeleteHandler(db))
 }
