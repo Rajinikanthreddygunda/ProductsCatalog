e916507447c874534ee94cf9fb98c8a4dac1f7c8
Rajinikanth Reddy <rajinikanthrdd37@gmail.com>
2025-03-13 10:38:49 -0500

    Updated business logic, controllers, routes, and database logic

--- Products/routes.go
+++ Products/routes.go
@@ -5,7 +5,18 @@ import (
 	"net/http"
 )
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	// Do nothing or return an error
+	http.Error(w, "Route not implemented", http.StatusNotImplemented)
+}
+
+// func RegisterRoutes(db *sql.DB) {
+// 	http.HandleFunc("/create", dummyHandler)
+// 	http.HandleFunc("/books", dummyHandler)
+// }
+
 func RegisterRoutes(db *sql.DB) {
-	h := NewHandler(db) //newline
-	http.HandleFunc("/products", h.CreateHandler(db))
+	//h := NewHandler(db)
+	http.HandleFunc("/products", testHandler)
+
 }
--- model/product.go
+++ model/product.go
@@ -1,6 +1,7 @@
 package model
 
 type Product struct {
+	ID          int     `json:"id"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
