3d04ef1a592aa7d6fe1e40f1481f0ad9d838bebd
Rajinikanth Reddy <158099970+Rajinikanthreddygunda@users.noreply.github.com>
2025-03-17 17:15:39 -0500

    Update productscatalog.go

--- database/productscatalog.go
+++ database/productscatalog.go
@@ -1,16 +1,3 @@
 package database
 
-import (
-	"database/sql"
-)
 
-// DeleteProduct removes a product from the database.
-func DeleteProduct(db *sql.DB, productID int) error {
-	// Delete the product record into the "products" table.
-	query := "DELETE FROM products WHERE id = ?"
-	_, err := db.Exec(query, productID)
-	if err != nil {
-		return err
-	}
-	return nil
-}
